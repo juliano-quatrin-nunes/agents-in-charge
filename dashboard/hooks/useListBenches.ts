6b344dd38a1f763f2c2082e18c62630117d9161e
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2024-12-09 10:39:50 -0300

    feat: change route for http requests to api

--- dashboard/hooks/useListBenches.ts
+++ dashboard/hooks/useListBenches.ts
@@ -1,10 +1,15 @@
-import { INITIAL_ENDPOINT } from "@/lib/utils";
+import { DEFAULT_HEADER, INITIAL_ENDPOINT } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
 
 const useListBenches = () => {
+  console.log(process.env.API_AUTHORIZATION_TOKEN);
   return useQuery({
     queryKey: ["allBenches"],
-    queryFn: () => fetch(INITIAL_ENDPOINT).then((res) => res.json()),
+    queryFn: () =>
+      fetch(INITIAL_ENDPOINT, {
+        method: "GET",
+        headers: DEFAULT_HEADER,
+      }).then((res) => res.json()),
   });
 };
 
--- dashboard/services/componentApiService.ts
+++ dashboard/services/componentApiService.ts
@@ -1,9 +1,15 @@
 import { RequestBody, ResponseData } from "@/lib/types";
+import { DEFAULT_HEADER } from "@/lib/utils";
 
-export const getComponentState = (endpoint: string): Promise<ResponseData> => fetch(endpoint).then((res) => res.json());
+export const getComponentState = (endpoint: string): Promise<ResponseData> =>
+  fetch(endpoint, {
+    method: "GET",
+    headers: DEFAULT_HEADER,
+  }).then((res) => res.json());
 
 export const postComponentState = (endpoint: string, data: RequestBody): Promise<ResponseData> =>
   fetch(endpoint, {
     body: JSON.stringify(data),
+    headers: DEFAULT_HEADER,
     method: "POST",
   }).then((res) => res.json());
